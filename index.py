13f9074ad39ad1fbd0bbae2abfc535ab949a1b89
Gert Boer <gertboer@gmail.com>
2019-01-07 22:33:59 +0100

    Experimented with Random Forest

--- index.py
+++ index.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
 
 # used as example: https://github.com/SusmeetJain/dengue_prediction/blob/master/1.Simple_Linear_Models.ipynb
 
@@ -31,14 +32,17 @@ def preprocess(data, train):
 	else:
 		data.fillna(0,inplace=True)
 
+	is_sj = data.city == 'sj'
 
 	months = data.week_start_date.apply(extract_month)
 
-	data.drop(['city', 'year', 'weekofyear', 'week_start_date'], axis=1, inplace=True)
+	data.drop(['city', 'weekofyear', 'week_start_date'], axis=1, inplace=True)
 
-	scaler = MinMaxScaler()
+	scaler = StandardScaler()
 
 	data[data.columns] = scaler.fit_transform(data)
+
+	data['is_sj'] = is_sj.loc[data.index]
 
 	sliced_months = months.loc[data.index]
 
@@ -48,27 +52,45 @@ def preprocess(data, train):
 	return data
 
 X = preprocess(X, True)
+
 y = y[['total_cases']]
 y = y.total_cases.loc[X.index]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, shuffle = False)
 
-
-lr = LinearRegression()
-lr.fit(X_train, Y_train)
-Y_pred = lr.predict(X_test)
-print(mean_absolute_error(Y_test, Y_pred))
-
 V = preprocess(V, False)
 
-print(V)
-predictions = lr.predict(V)
 
-# predictions = np.sqrt(predictions**2)
-# predictions = predictions.astype(int)
+# 0 = linear regression
+# 1 = random forrest
 
+algorithm = 1
+
+if algorithm == 0:
+	# Linear Regression
+	lr = LinearRegression()
+	lr.fit(X_train, Y_train)
+	Y_pred = lr.predict(X_test)
+	# print(mean_absolute_error(Y_test, Y_pred))
+
+	predictions = lr.predict(V)
+	predictions[predictions < 0] = 0
+
+elif algorithm == 1:
+	# Random forrest classifier
+	rf = RandomForestRegressor(n_estimators = 300)
+
+	rf.fit(X_train, Y_train);
+	Y_pred = rf.predict(X_test)
+	print(mean_absolute_error(Y_test, Y_pred))
+
+	predictions = rf.predict(V)
+
+
+predictions = predictions.astype(int)
 print(predictions)
 
+# Save predictions to file
 with open('predict', 'w') as csvfile2:
     writer=csv.writer(csvfile2, delimiter=',')
     writer.writerows(zip(predictions))
@@ -77,66 +99,4 @@ with open('predict', 'w') as csvfile2:
 exit()
 
 
-
-
-
-# Experimented with a Pytorch.
-
-# y_p = np.full(len(Y_test), np.mean(Y_train))
-# print(mean_absolute_error(Y_test, y_p))
-
-class LinearRegressionModel(nn.Module):
-
-	def __init__(self, input_dim, output_dim):
-
-		super(LinearRegressionModel, self).__init__() # its just like c++ super in inheritance bringing imp things to out class
-		self.linear = nn.Linear(input_dim, output_dim) # nn.linear is defined in nn.Module
-
-	def forward(self, x):# here its simple as we want our model to predict the output as what it thinks is correct
-
-		out = self.linear(x)
-
-		return out
-
-
-def train_model(model, inputs, labels, optimiser, criterion):
-	optimiser.zero_grad()
-
-	outputs = model.forward(inputs)
-	loss = criterion(outputs, labels)
-	loss.backward()# back props
-	optimiser.step()# update the parameters
-	print('epoch {}, loss {}'.format(epoch,loss.item()))
-
-
-def test_model(model, test_input):
-	print(model.forward(Variable(test_input)).data.numpy())
-
-
-
-input_dim = 20
-output_dim = 1
-
-model = LinearRegressionModel(input_dim,output_dim)
-
-criterion = nn.MSELoss()# Mean Squared Loss
-l_rate = 0.001
-optimiser = torch.optim.SGD(model.parameters(), lr = l_rate) 
-
-epochs = 2000
-
-
-X_train = Variable(torch.tensor(X_train)).float()
-Y_train = Variable(torch.tensor(Y_train)).float()
-
-
-
-for epoch in range(epochs):
-
-	train_model(model, X_train, Y_train, optimiser, criterion)
-
-	if epoch == 1 or epoch == 1500:
-		test_model(model, test_input)
-
-	
 
